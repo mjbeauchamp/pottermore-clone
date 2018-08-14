20ab8e263b1e1c6478f92e447635196edbbbf19c
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-08-14 11:45:58 -0600

    Set up file structure. Set up SCSS.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// Importing our main style sheet that holds all the styling
+import './styles/main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
