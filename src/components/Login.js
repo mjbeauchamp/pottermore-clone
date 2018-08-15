ef6aa346b2e8986caae427ee7e9e004b563f9f32
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-08-14 18:46:39 -0600

    Set up login and signup pages and their styling

---
+++ src/components/Login.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Navbar from './Navbar'
+
+//import {connect} from 'react-redux'
+//import {gatherUserId} from '../../ducks/reducer'
+
+class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    login =() => {
+        let {username, password} = this.state;
+        axios.post('/auth/login', {username: username, password: password})
+            .then(response => {
+                console.log("You logged in!")
+                // this.props.gatherUserId(username);
+                this.setState({
+                    username: '',
+                    password: ''
+                })
+                this.props.history.push('/pick_unicorn');
+            })
+            .catch(err => {
+                alert("Username and password incorrect.")
+                console.log(err)
+            });
+    }
+
+    onEnter = (e) => {
+        if(e.key==="Enter" && (this.state.username && this.state.password)){
+            this.login()
+        }
+    }
+
+    render(){
+        return (
+            <div onKeyDown={e => this.onEnter(e)}>
+                <Navbar />
+                <div className="auth-container">
+                    <h1 className="auth-title">Login</h1>
+                    <input
+                        className="auth-input"
+                        type="text"
+                        onChange={(e) => {this.setState({username: e.target.value})}} placeholder="Username" 
+                        value={this.state.username}
+                        autoFocus={true}/>
+
+                    <input
+                        className="auth-input"
+                        type="password"
+                        onChange={(e) => {this.setState({password: e.target.value})}} placeholder="Password" 
+                        value={this.state.password}/>
+
+                    <button className="auth-submit button" onClick={this.login}>Submit</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
---
+++ src/components/SignUp.js
@@ -0,0 +1,87 @@
+import React, {Component } from 'react';
+import axios from 'axios';
+import Navbar from "./Navbar"
+//import {connect} from 'react-redux'
+//import {gatherUserId} from '../../ducks/reducer'
+
+class SignUp extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    newUser = () => {
+        let {username, password} = this.state;
+        axios.get('/api/all_usernames')
+            .then(usernames => {
+                let nameArray = usernames.data.map(val => {
+                    return val.username
+                })
+                //Check to see if username already exists in the database
+                if(nameArray.indexOf(username) === -1){
+                    axios.post('/auth/new_user', {username: username, password: password})
+                        .then(response => {
+                            //Create a single default unicorn that all users have access to
+                            let newUserId = response.data[0].id;
+                            axios.post('/api/new_unicorn', {name: "Bubbles", file_name: "rainbow", user_id: newUserId})
+                                .then( response => {
+                        //Navigate back to the "Pick Your Unicorn" page
+                                    this.props.history.push('/pick_unicorn')
+                                })
+                                .catch(err => {console.log(err)})
+                            //Set the username and password fields to be blank
+                            this.setState({
+                            username: '',
+                            password: ''
+                            })
+                        })
+                        .catch(err => {
+                            console.log("Unable to create new user.",err)
+                        });
+                }  else{
+                    alert("Please choose a different username. This username is already taken.")
+                }
+            })
+            .catch(err => {
+                console.log("Unable to access username info.", err)
+            })
+    }
+
+    onEnter = (e) => {
+        if(e.key==="Enter" && (this.state.username && this.state.password)){
+            this.newUser()
+        }
+    }
+
+    render(){
+        return (
+            <div onKeyDown={e => this.onEnter(e)}>
+                <Navbar />
+                <div className="auth-container">
+                    <h1 className="auth-title">Sign Up</h1>
+                    <input 
+                        autoFocus={true} 
+                        className="auth-input" 
+                        type="text" 
+                        onChange={(e) => {this.setState({username: e.target.value})}} 
+                        placeholder="Username" 
+                        value={this.state.username}
+                        maxLength="25"/>
+                    <input 
+                        className="auth-input" 
+                        type="password" 
+                        onChange={(e) => {this.setState({password: e.target.value})}} 
+                        placeholder="Password" 
+                        value={this.state.password}
+                        maxLength="25"/>
+                    <button className="auth-submit button" onClick={this.newUser}>Submit</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
